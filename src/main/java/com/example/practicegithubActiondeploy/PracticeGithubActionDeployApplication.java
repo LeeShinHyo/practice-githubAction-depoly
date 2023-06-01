524cc9a480c38a47b440804bfbfc44844ba72154
LeeShinHyo <zdltlsgy@gmail.com>
2023-06-01 15:58:10 +0900

    3

--- src/main/java/com/example/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
+++ src/main/java/com/example/practicegithubActiondeploy/PracticeGithubActionDeployApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PracticeGithubActionDeployApplication {
-
 	public static void main(String[] args) {
+		System.out.println("");
 		SpringApplication.run(PracticeGithubActionDeployApplication.class, args);
 	}
 
